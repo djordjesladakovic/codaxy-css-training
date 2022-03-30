fe2fe8fbb8fa5c06314002482f2cf2d04fc746db
djordje.sladakovic <sladakovicdjordje@gmail.com>
2022-03-30 11:12:07 +0200

    Tryout with multiple items

--- app/layout/index.js
+++ app/layout/index.js
@@ -5,6 +5,22 @@ import Controller from "./Controller";
 export default (
     <cx>
         <div controller={Controller}>
+            <div class="main">
+                <div class="sidebar">
+                
+                </div>
+                <div class="container">
+                    <div class="item item1">
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div class="item item2"></div>
+                    <div class="item item3"></div>
+                    <div class="item item4"></div>
+                    <div class="item item5"></div>
+                    <div class="item item6"></div>
+                </div>
+            </div>
             <ContentPlaceholder />
         </div>
     </cx>

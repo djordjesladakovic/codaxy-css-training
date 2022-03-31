6b13b26a528bbb3580e754241c8e8258f67ead20
djordje.sladakovic <sladakovicdjordje@gmail.com>
2022-03-31 16:20:39 +0200

    add more style and content

--- app/layout/Controller.js
+++ app/layout/Controller.js
@@ -1,5 +1,34 @@
 import { Controller } from "cx/ui";
 
+var categories = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+];
 export default class extends Controller {
-    onInit() {}
+    onInit() {
+        var y1 = 0, y2 = 150;
+        this.store.set(
+            '$page.points', 
+            Array.from({length: 50}, 
+            (_, i) => ({
+                x: i * 3,
+                y: i % 20 == 3 ? null : (y1 = (y1 + (Math.random() - 0.5) * 30)),
+                y2: y2 = (y2 + (Math.random() - 0.5) * 30),
+                y2l: y2 - 50,
+                y2h: y2 + 50
+        }))); 
+
+        this.store.set(
+            "$page.pointsBar",
+            Array.from({ length: categories.length }, (_, i) => ({
+              x: categories[i],
+              v1: Math.random() * 30,
+              v2: Math.random() * 30,
+              v3: Math.random() * 30,
+            }))
+        );
+    }
 }
--- app/layout/index.js
+++ app/layout/index.js
@@ -1,6 +1,18 @@
 import { Button } from "cx/widgets";
-import { ContentPlaceholder } from "cx/ui";
+import { ContentPlaceholder, KeySelection, Repeater } from "cx/ui";
 import Controller from "./Controller";
+import { Svg } from "cx/svg";
+import {
+    Chart,
+    Gridlines,
+    LineGraph,
+    NumericAxis,
+
+    CategoryAxis,
+    ColumnGraph,
+    Column,
+    Legend,
+  } from "cx/charts";
 
 export default (
     <cx>
@@ -9,7 +21,8 @@ export default (
                 <div id="sidebar">
 
                 </div>
-                <div class="contents">  
+                <div class="contents"> 
+
                     <div class="content" id="content1">
                         <div class="picture"></div>
                         <div class="info">
@@ -18,14 +31,115 @@ export default (
                             <div style="margin-top: 24px;" class="detailed-info">
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor nesto nesto nesto nesto nesto123
                             </div>
-                            <Button baseClass="primaryButton" style="margin-top: 24px;">Build something</Button>
+                            <Button mod="primary" style="margin-top: 24px;">Build something</Button>
                         </div>
                     </div>
-                    <div class="content" id="content2">Content 2</div>
+
                     <div id="content-space"></div>
-                    <div class="content" id="content3">Content 3</div>
-                    <div class="content" id="content4">Content 4</div>
-                    <div class="content" id="content5">Content 5</div>
+
+                    <div class="content" id="content2">
+                        <div class="line-chart-details">
+                            <div class="header">Line chart</div>
+                            <div style="margin-top: 24px; -webkit-line-clamp: 3;" class="detailed-info">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor nesto nesto nesto nesto nesto123
+                            </div>
+                            <Button mod="secondary" style="margin-top: 36px;">See Analysis</Button>
+                        </div>
+                        <div class="chart">
+                            <Svg>
+                                <Chart offset="20 -10 -40 40" axes={{ x: { type: NumericAxis }, y: { type: NumericAxis, vertical: true } }}>
+                                    <Gridlines/>
+                                    <LineGraph name="Line 1" data-bind="$page.points" colorIndex={5} yField="y2h" active-bind="$page.line1"/>
+                                    <LineGraph name="Line 2" data-bind="$page.points" colorIndex={3} yField="y2" active-bind="$page.line2"/>  
+                                    <LineGraph name="Line 3" data-bind="$page.points" colorIndex={10} yField="y2l" active-bind="$page.line3"/>    
+                                </Chart>
+                            </Svg>
+                        </div>
+                    </div>
+
+                    <div class="content" id="content3">
+                        <div class="bulletpoint-text">
+                            Some random text instead of a really long title
+                        </div>
+                        <div class="bulletpoint-detail">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor nesto nesto nesto nesto nesto123
+                            sed do eiusmod tempor nesto nesto nesto nesto nesto123
+                            sed do eiusmod tempor nesto nesto nesto nesto nesto123
+                        </div>
+                        <Button mod="hollow">Read More</Button>
+                    </div>
+
+                    <div class="content" id="content4">
+                        <div class="bulletpoint-text">
+                                Some random text instead of a really long title
+                            </div>
+                            <div class="bulletpoint-detail">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor nesto nesto nesto nesto nesto123
+                                sed do eiusmod tempor nesto nesto nesto nesto nesto123
+                                sed do eiusmod tempor nesto nesto nesto nesto nesto123
+                            </div>
+                            <Button mod="hollow">Read More</Button>
+                        </div>
+                    <div class="content" id="content5">
+                        <div class="line-chart-details">
+                            <div class="header">Bar chart</div>
+                            <div style="margin-top: 24px; -webkit-line-clamp: 3;" class="detailed-info">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor nesto nesto nesto nesto nesto123
+                            </div>
+                            <Button mod="secondary" style="margin-top: 36px;">See Analysis</Button>
+                        </div>
+                        <div class="chart">
+                            <Svg>
+                                <Chart
+                                offset="20 -20 -40 40"
+                                axes={{
+                                    x: { type: CategoryAxis },
+                                    y: { type: NumericAxis, vertical: true, snapToTicks: 0 }
+                                }}
+                                >
+                                <Gridlines />
+                                <Repeater records:bind="$page.pointsBar" recordAlias="$point">
+                                    <Column
+                                    name="V1"
+                                    active:bind="$page.v1"
+                                    colorIndex={2}
+                                    width={0.3}
+                                    offset={-0.15}
+                                    x:bind="$point.x"
+                                    y:bind="$point.v1"
+                                    tooltip:tpl="V1 {$point.x} {$point.v1:n}"
+                                    stack="v"
+                                    stacked
+                                    />
+                                    <Column
+                                    name="V2"
+                                    active:bind="$page.v2"
+                                    colorIndex={1}
+                                    width={0.3}
+                                    offset={-0.15}
+                                    x:bind="$point.x"
+                                    y:bind="$point.v2"
+                                    tooltip:tpl="V2 {$point.x} {$point.v2:n}"
+                                    stack="v"
+                                    stacked
+                                    />
+                                    <Column
+                                    name="V3"
+                                    active:bind="$page.v3"
+                                    colorIndex={0}
+                                    width={0.3}
+                                    offset={-0.15}
+                                    x:bind="$point.x"
+                                    y:bind="$point.v3"
+                                    tooltip:tpl="V3 {$point.x} {$point.v3:n}"
+                                    stack="v"
+                                    stacked
+                                    />
+                                </Repeater>
+                                </Chart>
+                            </Svg>
+                        </div>
+                    </div>
                     <div class="content" id="content6">Content 6</div>
                 </div>
             </div>

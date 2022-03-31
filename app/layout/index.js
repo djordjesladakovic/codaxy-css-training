b20fff5922147a733548ea417bdbbf87684ccf3a
djordje.sladakovic <sladakovicdjordje@gmail.com>
2022-03-31 09:02:37 +0200

    Styling first content

--- app/layout/index.js
+++ app/layout/index.js
@@ -1,4 +1,4 @@
-import { Link } from "cx/widgets";
+import { Button, Link } from "cx/widgets";
 import { ContentPlaceholder } from "cx/ui";
 import Controller from "./Controller";
 
@@ -10,7 +10,17 @@ export default (
 
                 </div>
                 <div class="contents">  
-                    <div class="content" id="content1">Content 1</div>
+                    <div class="content" id="content1">
+                        <div class="picture"></div>
+                        <div class="info">
+                            <div style="font-size: 12px;">Author name</div>
+                            <div style="margin-top: 8px;" class="header">The best way to learn CSS is by building something</div>
+                            <div style="margin-top: 24px;" class="detailed-info">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor nesto nesto nesto nesto nesto123
+                            </div>
+                            <Button class="primary-button" style="margin-top: 24px;">Build something</Button>
+                        </div>
+                    </div>
                     <div class="content" id="content2">Content 2</div>
                     <div id="content-space"></div>
                     <div class="content" id="content3">Content 3</div>
